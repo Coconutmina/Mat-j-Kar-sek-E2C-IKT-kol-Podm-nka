bcd1815e4123856f95d07dcf642b3f6d808ff95c
matěj <matj@DESKTOP-1HI5AKA>
2021-10-23 13:20:22 +0200

    Přidejte soubory projektu.

---
+++ Matěj Karásek E2C IKT úkol Podmínka/Matěj Karásek E2C IKT úkol Podmínka.cpp
@@ -0,0 +1,19 @@
+﻿# include <stdio.h>
+
+int main(void) {
+
+	int x = 9;
+	int y = 7;
+
+	printf("cislo %d je ", x);
+
+	if (x < y) {
+		printf("mensi");
+	}
+	else {
+		printf("vetsi");
+	}
+	printf(" nez %d\n", y);
+}
+
+//Domácí ůkol z IKT na podmínku ve Visual Studio
